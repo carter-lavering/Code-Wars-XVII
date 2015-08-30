103239bcae01d28858fd8a6afb21c3ff6ea172eb
Carter Lavering <carterlego2000@gmail.com>
2015-08-29 18:24:44 -0700

    Completed 12

--- prob12.py
+++ prob12.py
@@ -1,4 +1,4 @@
-LETTERS = """..A..
+BANNER_ALPHABET = """..A..
 .A.A.
 .AAA.
 A...A
@@ -131,7 +131,18 @@ ZZZZZ"""
 
 ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+LETTERS_DICT = {
+    tuple(line.replace('.', ' ').replace(letter, '#')
+          for line in BANNER_ALPHABET.split('\n') if letter in line):
+    letter for letter in ALPHABET
+}
+
+SPACE = ('     ', '     ', '     ', '     ', '     ')
+LETTERS_DICT[SPACE] = ' '
+
+
 def mass_input(lines=None, end=None):
+    """Takes multiple lines of input."""
     list_of_input = []
     if lines is not None:
         for x in range(lines):
@@ -146,28 +157,60 @@ def mass_input(lines=None, end=None):
     return list_of_input
 
 
-def split_letters(string):
-    letters_dict = {
-        letter: [line for line in string.split('\n') if letter in line]
-        for letter in ALPHABET
-    }
-    return letters_dict
+def multiline_reversed(string):
+    return '\n'.join(''.join(reversed(line)) for line in string.split('\n'))
+
+
+def split_banners(multiple_line_banner):
+    """Splits multiple banners into a list."""
+    return [banner.strip('\n')
+            for banner in multiple_line_banner.split('\n\n')]
+
+
+def split_banner(banner):
+    """Splits a banner into letters."""
+    banner_lines = banner.split('\n')
+    # Make sure every line is the same
+    for x in range(len(banner_lines) - 1):
+        if len(banner_lines[x]) > len(banner_lines[x + 1]):
+            banner_lines[x + 1] += ' '
+        elif len(banner_lines[x]) < len(banner_lines[x + 1]):
+            banner_lines[x] += ' '
+    assert all(len(banner[x]) == len(banner[x + 1])
+               for x in range(len(banner_lines) - 1))
+    # Now the actual splitting part
+    letters = [
+        tuple(line[x:x + 5] for line in banner_lines)
+        for x in range(0, len(banner_lines[0]), 6)
+    ]
+    return letters
+
 
 def read_banner(banner):
-    banner = banner.split('\n')
+    """Reads a 5-line-high banner and returns a string."""
+    banner_letters = split_banner(banner)
+    return ''.join(
+        LETTERS_DICT[letter]
+        for letter in banner_letters
+        if letter != SPACE
+    )
 
+
+def read_boustrophedon(multiple_line_banner):
+    """Reads multiple lines of banners in alternating directions."""
+    list_of_banners = split_banners(multiple_line_banner)
+    text = []
+    for banner in list_of_banners:
+        try:
+            text.append(read_banner(banner))
+        except KeyError:
+            text.append(read_banner(multiline_reversed(banner)))
+    return ''.join(text)
 
 
 def main():
-    raw_i = mass_input(lines=int(input()) * 6)
-    output_string = ''
-    for b_line in split_banner(raw_i):  # banner_line
-        try:
-            s_line = read_banner(b_line)
-        except LookupError:
-            s_line = read_banner(backwards(b_line))
-        output_string += s_line
-    print(output_string)
+    raw_i = '\n'.join(mass_input(lines=int(input()) * 6))
+    print(read_boustrophedon(raw_i))
 
 
 if __name__ == '__main__':
